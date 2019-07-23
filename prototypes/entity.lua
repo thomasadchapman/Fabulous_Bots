c281293e421c3e8fc7a5515720c76728a33fd6a5
Marek <czbuchi@gmail.com>
2019-07-23 21:14:02 +0200

    "real" base code

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -1,13 +1,72 @@
-data:extend({
-    {
-        type = "item-entity",
-        name = "hard-hat",
-        icon = "__Fabulous_Bots__/graphics/icon_hard_hat.png",
-        flags = {"pushable", "placeable-neutral", "player-creation"},
-        flags = {"placeable-off-grid", "not-on-map"},
-        collision_box = {{-0.14, -0.14}, {0.14, 0.14}},
-        selection_box = {{-0.17, -0.17}, {0.17, 0.17}},
-        minable = {mining_time = 0.025}
-        inventory_size = 1
-    }
-  })
+local iconsPath = "__Fabulous-Bots__/graphics/icons/";
+local entityPath = "__Fabulous-Bots__/graphics/entity/";
+
+-- fabulous-construction-robot entity factory
+function createConstructionRobot(name) 
+    local robot = table.deepcopy(data.raw["construction-robot"]["construction-robot"]);
+
+    robot.name = name;
+    robot.icon = iconsPath .. "fabulous-construction-robot.png";
+
+    robot.idle.filename = entityPath .. name .. "/" .. name .. ".png";
+    robot.idle.hr_version.filename = entityPath .. name .. "/hr-" .. name .. ".png";
+
+    robot.in_motion.filename = entityPath .. name .. "/" .. name .. ".png";
+    robot.in_motion.hr_version.filename = entityPath .. name .. "/hr-" .. name .. ".png";
+
+    robot.shadow_idle.filename = entityPath .. name .. "/" .. name .. "-shadow.png";
+    robot.shadow_idle.hr_version.filename = entityPath .. name .. "/hr-" .. name .. "-shadow.png";
+
+    robot.shadow_in_motion.filename = entityPath .. name .. "/" .. name .. "-shadow.png";
+    robot.shadow_in_motion.hr_version.filename = entityPath .. name .. "/hr-" .. name .. "-shadow.png";
+
+    robot.working.filename = entityPath .. name .. "/" .. name .. "-working.png";
+    robot.working.hr_version.filename = entityPath .. name .. "/hr-" .. name .. "-working.png";
+
+    robot.shadow_working.filename = entityPath .. name .. "/" .. name .. "-shadow.png";
+    robot.shadow_working.hr_version.filename = entityPath .. name .. "/hr-" .. name .. "-shadow.png";
+
+    return robot;
+end
+
+-- fabulous-logistic-robot entity factory
+function createLogisticRobot(name)
+    local robot = table.deepcopy(data.raw["logistic-robot"]["logistic-robot"]);
+
+    robot.name = name;
+    robot.icon = iconsPath .. "fabulous-logistic-robot.png";
+
+    robot.idle.filename = entityPath .. name .. "/" .. name .. ".png";
+    robot.idle.hr_version.filename = entityPath .. name .. "/hr-" .. name .. ".png";
+
+    robot.in_motion.filename = entityPath .. name .. "/" .. name .. ".png";
+    robot.in_motion.hr_version.filename = entityPath .. name .. "/hr-" .. name .. ".png";
+
+    robot.shadow_idle.filename = entityPath .. name .. "/" .. name .. "-shadow.png";
+    robot.shadow_idle.hr_version.filename = entityPath .. name .. "/hr-" .. name .. "-shadow.png";
+
+    robot.shadow_in_motion.filename = entityPath .. name .. "/" .. name .. "-shadow.png";
+    robot.shadow_in_motion.hr_version.filename = entityPath .. name .. "/hr-" .. name .. "-shadow.png";
+
+    robot.idle_with_cargo.filename = entityPath .. name .. "/" .. name .. ".png";
+    robot.idle_with_cargo.hr_version.filename = entityPath .. name .. "/hr-" .. name .. ".png";
+
+    robot.in_motion_with_cargo.filename =entityPath .. name .. "/" .. name .. ".png";
+    robot.in_motion_with_cargo.hr_version.filename = entityPath .. name .. "/hr-" .. name .. ".png";
+
+    robot.shadow_idle_with_cargo.filename = entityPath .. name .. "/" .. name .. "-shadow.png";
+    robot.shadow_idle_with_cargo.hr_version.filename = entityPath .. name .. "/hr-" .. name .. "-shadow.png";
+
+    robot.shadow_in_motion_with_cargo.filename = entityPath .. name .. "/" .. name .. "-shadow.png";
+    robot.shadow_in_motion_with_cargo.hr_version.filename = entityPath .. name .. "/hr-" .. name .. "-shadow.png";
+
+    return robot; 
+end
+
+-- register new fabulous robot entities (needs to correspond with code in items.lua)
+data:extend {
+    createConstructionRobot("fabulous-construction-robot-1"),
+    createConstructionRobot("fabulous-construction-robot-2"),
+    createLogisticRobot("fabulous-logistic-robot-1"),
+    createLogisticRobot("fabulous-logistic-robot-2")
+};
--- prototypes/recipe.lua
+++ prototypes/recipe.lua
@@ -1,12 +1,34 @@
-data:extend({
-    {
-       type = "recipe",
-       name = "hard-hat",
-       enabled = "true",
-       ingredients = 
-       {
-         {"wood",5}
-       },
-       result = "hard-hat"
-     }
-   })
+
+-- recipe factory: vanilla robot + wood => fabulous robot
+function createRecipe(vanillaName, name, count)
+    local recipe = table.deepcopy(data.raw.recipe[vanillaName]);
+    recipe.name = name;
+    recipe.icon = "__Fabulous-Bots__/graphics/icons/" .. name .. ".png";
+    recipe.icon_size = 32;
+    recipe.subgroup = "other";
+    recipe.ingredients = {
+        { vanillaName, 1 },
+        { "wood", 1 },
+    };
+
+    -- result are evenly distributed for each fabulous robot
+    local results = {};
+    local probability = 1 / count;
+    for i = 1, count, 1
+    do
+        table.insert(results,  {
+            name = name .. "-" .. i,
+            probability = probability,
+            amount = 1
+        });
+    end
+
+    recipe.results = results;
+    return recipe;
+end
+
+-- register new fabulous robot recipes - count needs to correspond to code in entity.lua)
+data:extend {
+    createRecipe("construction-robot", "fabulous-construction-robot", 2),
+    createRecipe("logistic-robot", "fabulous-logistic-robot", 2)
+};
