4f4f7f7e321a7c6c274179a48752f837b1b5076d
Thomas Chapman <Thomas.Chapman@nielsen.com>
2019-07-23 10:11:30 +0100

    initial commit

---
+++ prototypes/item.lua
@@ -0,0 +1,12 @@
+data:extend({
+
+    {
+       type = "item",
+       name = "hard hat",
+       icon = "",
+       flags = {  },
+       subgroup = ,
+       place_result=,
+       stack_size= 1,
+     },   
+   })
